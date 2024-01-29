2b05c83c1775527877bd883305167e6b428813cb
VoxelFun <padolod@gmail.com>
2024-01-29 09:49:54 +0100

    Fix reading save data

--- src/utils/saves/SaveReader.ts
+++ src/utils/saves/SaveReader.ts
@@ -5,6 +5,13 @@ export class SaveReader {
     }
 
     read(): Save {
-        return JSON.parse(this.data);
+        //TODO: store save in a different format
+        const save: Save = JSON.parse(this.data);
+        Object.keys(save.balanceMap).forEach(key => {
+            save.balanceMap[key].values.forEach(value => {
+                value.ts = new Date(value.ts);
+            });
+        });
+        return save;
     }
 }
